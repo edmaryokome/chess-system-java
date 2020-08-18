216fe8546f0bd08b9768cd894db02a036358f3d9
Edmar A Yokome <edmar.yokome@gmail.com>
2020-08-18 15:57:43 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 	
-		System.out.println("Hello Wordl!");
+		//System.out.println("Hello Wordl!");
+		
+	   Position pos = new Position(3, 5);
+	   
+	   System.out.println(pos);
 		
 
 	}
